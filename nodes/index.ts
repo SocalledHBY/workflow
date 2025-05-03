0b0964a370e708192d28775fbd1acfe1898f3d3e
hby <socalledhby@gmail.com>
2025-05-04 05:43:35 +0800

    update homepage workflow

--- nodes/index.ts
+++ nodes/index.ts
@@ -5,17 +5,17 @@ export const initialNodes: BuiltInNode[] = [
     id: "a",
     type: "input",
     position: { x: 0, y: -100 },
-    data: { label: "1. Add modules on the left" }
+    data: { label: "1. 点击左侧按钮添加模块" }
   },
   {
     id: "b",
     position: { x: 0, y: 0 },
-    data: { label: "2. Connect modules in order" }
+    data: { label: "2. 连接模块" }
   },
   {
     id: "c",
     type: "output",
     position: { x: 0, y: 100 },
-    data: { label: "3. Run your own workflow" }
+    data: { label: "3. 运行工作流" }
   }
 ];
